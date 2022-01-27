ad6c9950a4007fe9f1790a989b3b798c74b6587a
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2022-01-28 00:03:06 +0100

    modification 0010

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -17,7 +17,10 @@ type Token = {
 
 export async function middleware(req: NextApiRequest) {
 	// Token will exist once user is logged in
-	const token: JWT = await getToken({ req, secret: process.env.JWT_SECRET });
+	const token = await getToken({
+		req,
+		secret: process.env.JWT_SECRET && process.env.JWT_SECRET,
+	});
 	// get pathname from nextUrl
 	const { pathname }: { pathname: string } = req.nextUrl;
 	// allow the request if below is true
